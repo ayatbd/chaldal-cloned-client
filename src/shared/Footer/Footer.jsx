aaa5684d4a7de1d6a8e06c2beb480e6890ee4195
A Behesti <albehestione@gmail.com>
2025-09-03 13:50:01 +0600

    completed footer top

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer-banner mx-[6.5%]">
+      <div className="banner">
+        <div className="w-[25%]">
+          <span>
+            <ul className="flex items-center justify-between">
+              <li className="text-xl font-bold">Cities:</li>
+              <li className="cityText underline">Dhaka</li>
+              <li className="cityText underline">Chattogram</li>
+              <li className="cityText underline">Jashore</li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
